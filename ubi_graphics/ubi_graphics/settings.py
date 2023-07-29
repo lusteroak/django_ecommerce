812e9f6e2abe10c116ac2ab4bbac6e3ba25ad41a
smontenegro2-admin <smontenegro.ex@meatcode.cl>
2023-07-29 13:09:30 -0400

    Updated files

--- ubi_graphics/ubi_graphics/settings.py
+++ ubi_graphics/ubi_graphics/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SESSION_COOKIE_AGE = 86400
+CART_SESSION_ID = 'cart'
 
 # Application definition
 
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     #apps
     'core',
     'product',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
