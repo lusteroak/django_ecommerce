7e1a9559b3e7bba2852a0e0ffee6b0759d984f28
smontenegro2-admin <smontenegro.ex@meatcode.cl>
2023-07-29 11:02:50 -0400

    add imageUrl to model

--- ubi_graphics/product/models.py
+++ ubi_graphics/product/models.py
@@ -15,6 +15,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     slug = models.SlugField()
+    imageUrl = models.CharField(max_length=255, null=True)
     description = models.TextField(blank=True, null=True)
     price = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
