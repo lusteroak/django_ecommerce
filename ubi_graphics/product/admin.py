6af2445ed576c41834d850501465f11a3eb2e9b8
Samuel Montenegro <lusteroak@protonmail.com>
2023-07-29 09:55:10 -0400

    add product app

---
+++ ubi_graphics/product/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Category, Product
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+# Register your models here.
