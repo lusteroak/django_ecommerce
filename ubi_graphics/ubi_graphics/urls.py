1780d87a2ad666d40b9025f96fd01efa0970aaaf
Samuel Montenegro <lusteroak@protonmail.com>
2023-07-30 19:00:43 -0400

    add login view in urls.py

--- ubi_graphics/ubi_graphics/urls.py
+++ ubi_graphics/ubi_graphics/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('signup/', signup, name='signup'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('login/', login_old, name='login'),
+    path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>', product, name='product'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
