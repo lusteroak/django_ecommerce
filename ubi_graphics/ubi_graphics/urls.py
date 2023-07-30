de5cab1df2b8c9d397691447f84c98087b6859a3
smontenegro2-admin <smontenegro.ex@meatcode.cl>
2023-07-29 23:01:13 -0400

    add signup and login

--- ubi_graphics/ubi_graphics/urls.py
+++ ubi_graphics/ubi_graphics/urls.py
@@ -1,14 +1,20 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views
 from django.urls import path
 
 from cart.views import add_to_cart
-from core.views import frontpage, shop
+from core.views import frontpage, shop, signup, login_old
 from product.views import product
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
+    path('signup/', signup, name='signup'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('login/', login_old, name='login'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>', product, name='product'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
