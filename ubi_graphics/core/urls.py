eb83e29792759276af1e57effdf47cb9b9195869
Samuel Montenegro <lusteroak@protonmail.com>
2023-07-30 23:48:36 -0400

    add myaccount and editaccount urls

--- ubi_graphics/core/urls.py
+++ ubi_graphics/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views
 
-from core.views import frontpage, shop, signup
+from core.views import frontpage, shop, signup, my_account, edit_my_account
 
 from product.views import product
 
@@ -12,4 +12,6 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>', product, name='product'),
+    path('myaccount/', my_account, name='my_account'),
+    path('editaccount/', edit_my_account, name='edit_account'),
 ]
